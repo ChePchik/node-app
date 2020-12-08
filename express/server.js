da1d04f165e8eef790811deb8848bc9f65196599
Egorium <egor.anyin@yandex.ru>
2020-12-08 23:43:15 +0300

    server new

--- express/server.js
+++ express/server.js
@@ -6,27 +6,19 @@ const app = express();
 const fs = require("fs");
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<a href="/api>api</h1>');
-    res.end();
+app.use(bodyParser.json());
+// app.use('/.netlify/functions/server', router); // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.get('/', (req, res) => {
+    res.send("users");
 });
-
 app.get('/api', (req, res) => {
 
     var content = fs.readFileSync(__dirname + "/todos.json", "utf8");
     var users = JSON.parse(content);
     res.send(users);
 });
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router); // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
 module.exports = app;
 module.exports.handler = serverless(app);
