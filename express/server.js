2a71de152ab997f2cb1905dbb8509f2b239536f7
Egorium <egor.anyin@yandex.ru>
2020-12-08 22:28:56 +0300

    up serv

--- express/server.js
+++ express/server.js
@@ -3,20 +3,30 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
+const fs = require("fs");
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<a href="/api>api</h1>');
+    res.end();
 });
+
+app.get('/api', (req, res) => {
+
+    var content = fs.readFileSync(__dirname + "/todos.json", "utf8");
+    var users = JSON.parse(content);
+    res.send(users);
+});
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/.netlify/functions/server', router); // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var Port = process.env.PORT || 5051;
 const app = require('./express/server');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(Port, () => console.log('\x1b[33m%s\x1b[0m', `На хосте http://localhost:${Port}/`));
