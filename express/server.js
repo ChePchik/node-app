202e910a2fb1ab41c7233705bcd92bea47997da0
Egorium <egor.anyin@yandex.ru>
2020-12-10 20:15:07 +0300

    fix fs

--- express/server.js
+++ express/server.js
@@ -18,12 +18,13 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 router.get('/api', (req, res) => {
-    var content = fs.readFileSync(__dirname + "/todos.json", "utf8");
+    var content = fs.readFileSync(path.join(__dirname, './todos.json'), "utf8"); //(__dirname + "/todos.json", "utf8");
     var users = JSON.parse(content);
     res.send(users);
 });
+
 app.get('/apis', (req, res) => {
-    var content = fs.readFileSync(__dirname + "/todos.json", "utf8");
+    var content = fs.readFileSync(path.join(__dirname, './todos.json'), "utf8"); //(__dirname + "/todos.json", "utf8");
     var users = JSON.parse(content);
     res.send(users);
 });
